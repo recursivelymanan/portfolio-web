e9f15f36c1d3452e91d2588d7e15388a28979a22
recursivelymanan <m1chopra@ucsd.edu>
2022-07-14 01:49:50 -0700

    final touches to construction page

--- src/App.js
+++ src/App.js
@@ -7,23 +7,31 @@ import IconLink from "./IconLink/IconLink";
 
 function App() {
   return (
-    <div className="root">
-      <h1 class="construction-msg">
-        {" "}
-        Hello! My site is currently under construction.{" "}
-      </h1>
-      <div className="icons">
-        <a href="https://twitter.com/literatureeee_">
-          <AiFillTwitterCircle size={50} style={{ color: "black" }} />
-        </a>
-        <a href="https://github.com/recursivelymanan">
-          <AiFillGithub size={50} style={{ color: "black" }} />
-        </a>
-        <a href="https://www.linkedin.com/in/manan-chopra22/">
-          <AiFillLinkedin size={50} style={{ color: "black" }} />
-        </a>
+    <>
+      <title>Manan Chopra</title>
+      <div className="root">
+        <h1 class="construction-msg">
+          {" "}
+          Hello! My site is currently under construction.{" "}
+        </h1>
+        <div className="socials">
+          <div className="icons">
+            <a href="https://twitter.com/literatureeee_">
+              <AiFillTwitterCircle size={50} style={{ color: "black" }} />
+            </a>
+            <a href="https://github.com/recursivelymanan">
+              <AiFillGithub size={50} style={{ color: "black" }} />
+            </a>
+            <a href="https://www.linkedin.com/in/manan-chopra22/">
+              <AiFillLinkedin size={50} style={{ color: "black" }} />
+            </a>
+          </div>
+          <div className="icons-msg">
+            <p>Check out my other profiles!</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 

5952b8f0ef9526260b1f7532e0015fd76b4aab94
recursivelymanan <m1chopra@ucsd.edu>
2022-07-13 21:08:58 -0700

    under construction message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./NavBar/NavBar";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import IconLink from "./IconLink/IconLink";
 
 function App() {
   return (
-    <div>
-      <h1>website is working !</h1>
+    <div className="root">
+      <h1 class="construction-msg">
+        {" "}
+        Hello! My site is currently under construction.{" "}
+      </h1>
+      <div className="icons">
+        <a href="https://twitter.com/literatureeee_">
+          <AiFillTwitterCircle size={50} style={{ color: "black" }} />
+        </a>
+        <a href="https://github.com/recursivelymanan">
+          <AiFillGithub size={50} style={{ color: "black" }} />
+        </a>
+        <a href="https://www.linkedin.com/in/manan-chopra22/">
+          <AiFillLinkedin size={50} style={{ color: "black" }} />
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+import { Link } from "react-scroll";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link activeClass="active" smooth spy to="about">
+            MANAN CHOPRA
+          </Link>
+        </li>
+        <li>
+          <Link activeClass="active" smooth spy to="projects">
+            PROJECTS
+          </Link>
+        </li>
+        <li>
+          <Link activeClass="active" smooth spy to="blog">
+            RESUME
+          </Link>
+        </li>
+        <li>
+          <Link activeClass="active" smooth spy to="contact">
+            CONTACT ME
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;

2b749cbc14437f41b9bc2743ba5645ae2886764b
recursivelymanan <m1chopra@ucsd.edu>
2022-08-08 15:35:30 -0700

    fixed link opening, added lab link

--- src/Socials/Socials.js
+++ src/Socials/Socials.js
@@ -2,18 +2,38 @@ import "./Socials.css";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
+import {ImLab} from "react-icons/im"
 
 function Socials() {
   return (
     <div className="socials">
       <div className="icons">
-        <a href="https://twitter.com/literatureeee_">
+      <a
+          href="http://wahlinlab.ucsd.edu/meet-wahling-lab/manan-chopra"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ImLab size={53} style={{ color: "black" }} />
+        </a>
+        <a
+          href="https://twitter.com/literatureeee_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <AiFillTwitterCircle size={60} style={{ color: "black" }} />
         </a>
-        <a href="https://github.com/recursivelymanan">
+        <a
+          href="https://github.com/recursivelymanan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <AiFillGithub size={60} style={{ color: "black" }} />
         </a>
-        <a href="https://www.linkedin.com/in/manan-chopra22/">
+        <a
+          href="https://www.linkedin.com/in/manan-chopra22/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <AiFillLinkedin size={60} style={{ color: "black" }} />
         </a>
       </div>

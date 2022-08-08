d1d2783b77c32c571dc418a3c3e8e8df83e500d5
recursivelymanan <m1chopra@ucsd.edu>
2022-08-08 15:19:49 -0700

    fixed navbar scrolling

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -10,22 +10,22 @@ function NavBar() {
           <AiFillCaretRight size={15} style={{ color: "black" }} />
         </li>
         <li>
-          <Link activeClass="active" smooth spy to="about" offset={-70}>
+          <Link activeClass="active" smooth spy to="about" offset={-65}>
             MANAN CHOPRA
           </Link>
         </li>
         <li>
-          <Link activeClass="active" smooth spy to="projects" offset={-70}>
+          <Link activeClass="active" smooth spy to="projects" offset={-65}>
             PROJECTS
           </Link>
         </li>
         <li>
-          <Link activeClass="active" smooth spy to="resume" offset={-70}>
+          <Link activeClass="active" smooth spy to="resume" offset={-65}>
             RESUME
           </Link>
         </li>
         <li>
-          <Link activeClass="active" smooth spy to="contact" offset={-70}>
+          <Link activeClass="active" smooth spy to="contact" offset={-65}>
             CONTACT ME
           </Link>
         </li>

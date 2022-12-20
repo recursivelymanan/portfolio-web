c59459a89733588732752ba57138de75a1057790
Manan Chopra <m1chopra@ucsd.edu>
2022-12-20 15:01:22 -0800

    work

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import "./App.css";
-import NavBar from "./NavBar/NavBar";
-import { AiFillTwitterCircle } from "react-icons/ai";
-import { AiFillGithub } from "react-icons/ai";
-import { AiFillLinkedin } from "react-icons/ai";
-import IconLink from "./IconLink/IconLink";
+import React, { Text } from "react";
+import CodeBlock from "./components/CodeBlock/CodeBlock";
+import CodeText from "./components/CodeText/CodeText";
+import MarkdownBlock from "./components/MarkdownBlock/MarkdownBlock";
+import MarkdownText from "./components/MarkdownText/MarkdownText";
+import OutputBlock from "./components/OutputBlock/OutputBlock";
+import OutputText from "./components/OutputText/OutputText";
 
 function App() {
-  return (
-    <>
-      <title>Manan Chopra</title>
-      <div className="root">
-        <h1 class="construction-msg">
-          {" "}
-          Hello! My site is currently under construction.{" "}
-        </h1>
-        <div className="socials">
-          <div className="icons">
-            <a
-              href="https://twitter.com/literatureeee_"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <AiFillTwitterCircle size={50} style={{ color: "black" }} />
-            </a>
-            <a
-              href="https://github.com/recursivelymanan"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <AiFillGithub size={50} style={{ color: "black" }} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/manan-chopra22/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <AiFillLinkedin size={50} style={{ color: "black" }} />
-            </a>
-          </div>
-          <div className="icons-msg">
-            <p>Check out my other profiles!</p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <p>hello world</p>;
 }
 
-export default App;
+export default App();
---
+++ src/components/MarkdownBlock/MarkdownBlock.js
@@ -0,0 +1,5 @@
+import React, { Text } from "react";
+
+export default function MarkdownBlock() {
+  return <Text>Test</Text>;
+}

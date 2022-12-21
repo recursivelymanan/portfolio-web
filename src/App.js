43438ac84dcedec5916f7cf3849dc23d29fdb7e2
Manan Chopra <m1chopra@ucsd.edu>
2022-12-20 17:26:47 -0800

    x

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Text } from "react";
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
-import Contact from "./components/Contact/Contact";
-import Socials from "./components/Socials/Socials";
-import DownloadButton from "./components/DownloadButton/DownloadButton";
-import Projects from "./components/Projects/Projects";
 
 export default function App() {
   return <p>hello world</p>;

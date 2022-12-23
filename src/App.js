59f7fa6cf1f45032c7a302b276914a117010aeb2
Manan Chopra <m1chopra@ucsd.edu>
2022-12-22 22:56:43 -0800

    updated about me

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Contact from "./components/Contact/Contact";
 import Socials from "./components/Socials/Socials";
 import DownloadButton from "./components/DownloadButton/DownloadButton";
 import Projects from "./components/Projects/Projects";
+import hm1 from "./imgs/hm1.png";
+import hm2 from "./imgs/hm2.png";
+import vp1 from "./imgs/vp1.png";
 
 function App() {
   return (
@@ -13,13 +16,47 @@ function App() {
       <header className="nav">
         <NavBar className="nav" />
       </header>
-      <section id="about"></section>
+      <section id="about">
+        <div className="about_section">
+          <div className="about_text">
+            <h3 className="about_intro">Hello! My name is</h3>
+            <h1 className="about_name">Manan Chopra.</h1>
+            <h3 className="about_tagline">
+              I use code to paint pictures from NGS data.
+            </h3>
+            <h3 className="about_tagline"> (Like these) </h3>
+          </div>
+        </div>
+        <div className="about_figs">
+          <a
+            href="https://github.com/WahlinLab/Human_RGC-iN/tree/main/Figure%204/Panel%20D/out"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="about_hm1" src={hm1} alt="heatmap" />
+          </a>
+          <a
+            href="https://github.com/WahlinLab/Human_RGC-iN/tree/main/Figure%204/Panel%20H/out"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="about_vp1" src={vp1} alt="volcano plot" />
+          </a>
+          <a
+            href="https://github.com/WahlinLab/Human_RGC-iN/tree/main/Figure%205/Panel%20F/out"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="about_hm2" src={hm2} alt="heatmap" />
+          </a>
+        </div>
+      </section>
       <section id="projects">
         <Projects />
       </section>
       <section id="resume">
         <div>
-          <h2>Download my resume here!</h2>
+          <h2 className="resume_text">Download my resume here!</h2>
           <DownloadButton />
         </div>
       </section>
@@ -27,7 +64,15 @@ function App() {
         <Contact />
       </section>
       <section id="footer">
-        <span id="footer-note"> hand coded by manan chopra in la jolla </span>
+        <span id="footer-note">
+          <a
+            href="https://github.com/recursivelymanan/portfolio-web"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hand coded by manan chopra in la jolla
+          </a>
+        </span>
       </section>
       <Socials />
     </>

86763ff984acbf9b70f5ffdcc213559371f0fe02
Manan Chopra <m1chopra@ucsd.edu>
2024-09-05 10:32:48 -0700

    Removed lab link

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,20 +1,12 @@
 import "./Socials.css";
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
-import { ImLab } from "react-icons/im";
 import React from "react";
 
 function Socials() {
   return (
-    <div className="socials">
-      <div className="icons">
-        <a
-          href="http://wahlinlab.ucsd.edu/meet-wahling-lab/manan-chopra"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <ImLab className="icon" size={53} style={{ color: "black" }} />
-        </a>
+    <div>
+      <div className="socials">
         <a
           href="https://github.com/recursivelymanan"
           target="_blank"

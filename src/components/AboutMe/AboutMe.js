f9d646e331d9a9b765635778b030dfc76cd4a506
Manan <manan.zchopra@gmail.com>
2025-01-18 13:21:25 -0800

    Minor text change

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,10 +9,12 @@ export default function AboutMe() {
           <h3 className="about_intro">Hello! My name is</h3>
           <h1 className="about_name">Manan Chopra.</h1>
           <h3 className="about_tagline">
-            I'm a bioinformatics engineer with a passion for writing stories with data.
+            I'm a bioinformatics engineer with a passion for writing stories
+            with data.
           </h3>
           <h3 className="about_tagline">
-            I'm still working on developing this website, but for now you can see some of my publications and presentations below, as well as my resume. 
+            I am continually adding to this website, for now you can see some of
+            my publications and presentations below, as well as my resume.
           </h3>
         </div>
       </div>

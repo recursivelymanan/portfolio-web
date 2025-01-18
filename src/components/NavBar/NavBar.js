5e608f2e903746f957c87a65906b8ffc0c9ecb42
Manan <manan.zchopra@gmail.com>
2025-01-18 13:20:00 -0800

    NavBar conditionally displays link based on current page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,34 +1,73 @@
-import { Link } from "react-scroll";
+import { Link as ScrollLink } from "react-scroll";
+import { useLocation, Link as RouterLink } from "react-router-dom";
 import { AiFillCaretRight } from "react-icons/ai";
+
 import "./NavBar.css";
 import React from "react";
 
 function NavBar() {
+  const location = useLocation();
+
   return (
     <nav id="navbar">
       <ul>
         <li id="nav-icon">
           <AiFillCaretRight size={15} style={{ color: "black" }} />
         </li>
+        {location.pathname === "/" ? (
+          <>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                smooth
+                spy
+                to="about"
+                offset={-65}
+              >
+                MANAN CHOPRA
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                smooth
+                spy
+                to="projects"
+                offset={-65}
+              >
+                PROJECTS
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                smooth
+                spy
+                to="resume"
+                offset={-65}
+              >
+                RESUME
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                smooth
+                spy
+                to="contact"
+                offset={-65}
+              >
+                CONTACT ME
+              </ScrollLink>
+            </li>
+          </>
+        ) : (
+          <li>
+            <RouterLink to="/">HOME</RouterLink>
+          </li>
+        )}
         <li>
-          <Link activeClass="active" smooth spy to="about" offset={-65}>
-            MANAN CHOPRA
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active" smooth spy to="projects" offset={-65}>
-            PROJECTS
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active" smooth spy to="resume" offset={-65}>
-            RESUME
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active" smooth spy to="contact" offset={-65}>
-            CONTACT ME
-          </Link>
+          <RouterLink to="/movies">MOVIES</RouterLink>
         </li>
       </ul>
     </nav>
